715763b715c86dacb3e8b72a38cb35ed4bc433e8
Tobias Kunze <rixx@cutebit.de>
2016-10-22 22:10:15 +0200

    Add domain to title output

--- shackbot/plugins/title.py
+++ shackbot/plugins/title.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlsplit
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -11,11 +13,15 @@ def title(parsed, user, target, text):
     if 'http://' in text or 'https://' in text:
         try:
             url = text[text.find('http'):].split()[0]
+            domain = "{0.scheme}://{0.netloc}/".format(urlsplit(url))
+
             response = requests.get(url, allow_redirects=True, timeout=2)
             response.raise_for_status()
+
             soup = BeautifulSoup(response.content, 'html.parser')
             title = soup.title.string
             title = title[:400] + '…' if len(title) > 400 else title
-            bot.say(target, 'Title: {}'.format(title))
+
+            bot.say(target, 'Title: {title} (at {domain})'.format(title=title, domain=domain))
         except:
             pass
