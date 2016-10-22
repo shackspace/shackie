22e21de6c107a9c72b743623c61c35990878f0ba
Andreas Madsack <andreas@madflex.de>
2016-10-22 17:58:28 +0200

    react only on .status. for now. more to come

--- shackbot/main.py
+++ shackbot/main.py
@@ -8,12 +8,8 @@ bot = irc.connect("chat.freenode.net", 6667, use_ssl=False)\
 
 @bot.on("message")
 def incoming_message(parsed, user, target, text):
-    bot.say(target, "{}: you said {}".format(user.nick, text))
-
-
-@bot.on("join")
-def on_join(message, user, channel):
-    bot.say(channel, "Hi {}! You're connecting from {}.".format(user.nick, user.host))
+    if text.startswith('.status'):
+        bot.say(target, "everything is ok, {}".format(user.nick))
 
 
 asyncio.get_event_loop().run_forever()
