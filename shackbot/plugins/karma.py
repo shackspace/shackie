b73ef87192b35c53822da7d2b1f005f02b27cdb3
Tobias Kunze <rixx@cutebit.de>
2016-10-23 14:41:15 +0200

    Print either top karma or requested karma

--- shackbot/plugins/karma.py
+++ shackbot/plugins/karma.py
@@ -1,3 +1,6 @@
+import json
+import operator
+
 from bot import Bot
 from registry import bot_command
 from storage import store
@@ -7,35 +10,44 @@ bot = Bot()
 
 
 @bot_command('karma')
-def karma(parsed, user, target, text):
+def show_karma(parsed, user, target, text):
     text = text[len('.karma'):].strip()
 
     if not text:
-        # print top/bottom
-        pass
+        top = json.loads(store.get('karma.top.{}'.format(target)).decode()) or dict()
+        bottom = json.loads(store.get('karma.bottom.{}'.format(target)).decode()) or dict()
+        user_karma = get_karma(target, user.nick)
 
-    to_print = []
-    in_literal = []
-    for word in text.split():
-        if in_literal:
-            if word.endswith('"'):
-                in_literal.append(word[:-1])
-                to_print.append(' '.join(in_literal))
-                in_literal = []
+        response = 'Top karma: {}. Flop karma: {}. Karma for {}: {}'.format(
+            ', '.join('{}: {}'.format(key, value) for key, value in top.items()),
+            ', '.join('{}: {}'.format(key, value) for key, value in bottom.items()),
+            user.nick, user_karma,
+        )
+        bot.say(target, response)
+
+    else:
+        to_print = []
+        in_literal = []
+        for word in text.split():
+            if in_literal:
+                if word.endswith('"'):
+                    in_literal.append(word[:-1])
+                    to_print.append(' '.join(in_literal))
+                    in_literal = []
+                else:
+                    in_literal.append(word)
+
+            elif word.startswith('"'):
+                if word.endswith('"'):
+                    to_print.append(word[1:-1])
+                else:
+                    in_literal.append(word[1:])
+
             else:
-                in_literal.append(word)
+                to_print.append(word)
 
-        elif word.startswith('"'):
-            if word.endswith('"'):
-                to_print.append(word[1:-1])
-            else:
-                in_literal.append(word[1:])
-
-        else:
-            to_print.append(word)
-
-    response = ', '.join('{}: {}'.format(word, get_karma(target, word)) for word in to_print)
-    bot.say(target, response)
+        response = ', '.join('{}: {}'.format(word, get_karma(target, word)) for word in to_print)
+        bot.say(target, response)
 
 
 @bot.on('message')
