8dd36b60b3aa6d1105d8073df974738ee83eb9bf
Tobias Kunze <rixx@cutebit.de>
2016-10-23 17:32:35 +0200

    Add replace plugin
    Thanks to the krebs guys:
    http://cgit.cd.krebsco.de/stockholm/tree/krebs/5pkgs/Reaktor/scripts/sed-plugin.py

---
+++ shackbot/plugins/replace.py
@@ -0,0 +1,45 @@
+"""
+via http://cgit.cd.krebsco.de/stockholm/tree/krebs/5pkgs/Reaktor/scripts/sed-plugin.py
+"""
+import re
+from subprocess import Popen, PIPE
+
+from bot import Bot
+from storage import store
+
+
+bot = Bot()
+
+
+def is_replace(line):
+    myre = re.compile(r'^s/((?:\\/|[^/])+)/((?:\\/|[^/])*)/([ig]*)$')
+    return myre.match(line)
+
+
+@bot.on('message')
+def replace_entrypoint(parsed, user, target, text):
+    regex = is_replace(text)
+    if regex:
+        last_line = store.get('replace.{}.{}'.format(target, user.nick)).decode()
+        if last_line:
+            source, destination, flagstr = regex.groups()
+            source = source.replace('\/','/')
+            destination = destination.replace('\/','/')
+            flagstr = flagstr or ''
+
+            proc = Popen(
+                ['sed', 's/{}/{}/{}'.format(source, destination, flagstr)],
+                stdin=PIPE, stdout=PIPE
+            )
+            out, error = proc.communicate(bytes("{}\n".format(last_line), "UTF-8"))
+            if proc.returncode:
+                bot.say(target, "Something went wrong when trying to process your regex: {}".format(error.decode()))
+                return
+
+            result = out.decode()
+            bot.say(target, "{} meinte: {}".format(user.nick, ret.strip()))
+
+            if result:
+                store.set('replace.{}.{}'.format(target, user.nick), result)
+    else:
+        store.set('replace.{}.{}'.format(target, user.nick), text)
