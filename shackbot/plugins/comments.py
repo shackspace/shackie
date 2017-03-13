61639859771c9823ab35bfd3bdb169b2e72433ae
Tobias Kunze <rixx@cutebit.de>
2017-03-13 22:08:52 +0100

    Fix answering

--- shackbot/plugins/comments.py
+++ shackbot/plugins/comments.py
@@ -7,7 +7,7 @@ from .comment_data import ANSWERS, PERSONAL_ANSWERS
 bot = Bot()
 
 
-def _send_answer(answer):
+def _send_answer(answer, target):
     answer = random.choice(answer['answers'])
     if answer.startswith('/me'):
         answer = answer[4:]
@@ -28,11 +28,11 @@ def comment(parsed, user, target, text):
         for answer in PERSONAL_ANSWERS:
             if answer['regex'].fullmatch(message):
                 if random.random() <= answer['probability']:
-                    _send_answer(answer)
+                    _send_answer(answer, target)
                     return
 
     for answer in ANSWERS:
         if answer['regex'].fullmatch(message):
             if random.random() <= answer['probability']:
-                _send_answer(answer)
+                _send_answer(answer, target)
                 return
