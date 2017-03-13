5b9024cd5e78e98851d64a5e58b9bde381fb4a6e
Tobias Kunze <rixx@cutebit.de>
2017-03-13 21:59:16 +0100

    Allow to answer to specific regexes

---
+++ shackbot/plugins/comments.py
@@ -0,0 +1,38 @@
+import random
+
+import config
+from bot import Bot
+from .comment_data import ANSWERS, PERSONAL_ANSWERS
+
+bot = Bot()
+
+
+def _send_answer(answer):
+    answer = random.choice(answer['answers'])
+    if answer.startswith('/me'):
+        answer = answer[4:]
+        bot.writeln('PRIVMSG {target} :\x01ACTION {answer}\x01'.format(target=target, answer=answer))
+    else:
+        bot.say(target, answer)
+
+
+@bot.on('message')
+def comment(parsed, user, target, text):
+    message = text
+    addressed = False
+    if message.startswith(config.NICKNAME + ': ') or message.startswith(config.NICKNAME + ', '):
+        message = message[len(config.NICKNAME) + 2:]
+        addressed = True
+
+    if addressed:
+        for answer in PERSONAL_ANSWERS:
+            if answer['regex'].fullmatch(message):
+                if random.random() <= answer['probability']:
+                    _send_answer(answer)
+                    return
+
+    for answer in ANSWERS:
+        if answer['regex'].fullmatch(message):
+            if random.random() <= answer['probability']:
+                _send_answer(answer)
+                return
