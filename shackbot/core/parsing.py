53aa918d930b392bdd0b8979600f11758d311f49
Tobias Kunze <rixx@cutebit.de>
2017-03-21 10:53:44 +0100

    [core] Fix off-by-one

--- shackbot/core/parsing.py
+++ shackbot/core/parsing.py
@@ -6,10 +6,10 @@ def get_command_if_bot_message(message, nickname, bot_char):
     if message.startswith(nickname + ': ') or message.startswith(nickname + ', '):
         message = message[len(nickname) + len(': ')]
         command = message.split()[0]
+        if command.startswith(bot_char):
+            return command[1:]
+        return command
+
     elif message.startswith(bot_char):
         message = message[1:]
-    else:
-        return None
-    command = message.split()[0]
-    if len(command) > 1:
-        command = command[1:]
+        return message.split()[0]
